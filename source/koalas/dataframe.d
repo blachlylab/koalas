39a3bb43209ca3a179b65e1f30d414deba4ad364
Charles Gregory <charles.gregory940@topper.wku.edu>
2020-08-28 12:15:44 -0400

    can now create empty df, add new columns, and print dataframe

--- source/koalas/dataframe.d
+++ source/koalas/dataframe.d
@@ -8,7 +8,7 @@ import std.array: split,join;
 import std.stdio;
 import std.meta: AliasSeq;
 import std.conv: to;
-import std.traits: isSomeChar;
+import std.traits: isSomeChar, isArray, isSomeString;
 
 struct Dataframe(RT){
     RT[] records;
@@ -17,6 +17,9 @@ struct Dataframe(RT){
         this.records = rows;
     }
 
+    /// returns a dataframe of filtered records
+    /// filters on col == value
+    /// similar to df[df["col"] == value] in pandas
     template select(string col){
         auto select(E)(E val) if(__traits(hasMember, RT, col))
         {
@@ -26,6 +29,9 @@ struct Dataframe(RT){
         }
     }
 
+    /// returns a dataframe of filtered records
+    /// filters on col != value
+    /// similar to df[df["col"] != value] in pandas
     template invertedSelect(string col){
         auto select(E)(E val) if(__traits(hasMember, RT, col))
         {
@@ -35,7 +41,9 @@ struct Dataframe(RT){
         }
     }
 
-
+    /// Returns an array of T items that are of the same
+    /// type as the col type in question
+    /// similar to df["col"] in pandas
     auto getCol(string col)() if(__traits(hasMember, RT, col))
     {
         mixin("alias T = typeof(RT." ~ col ~");");
@@ -46,6 +54,8 @@ struct Dataframe(RT){
         return ret;
     }
     
+    /// Assigns a column using an array or singluar type 
+    /// similar to df["col"] == val in pandas
     template setCol(string col){
         void setCol(E)(E[] arr) if(__traits(hasMember, RT, col) && !isSomeChar!E)
         {
@@ -55,7 +65,7 @@ struct Dataframe(RT){
                 mixin("rec."~ col ~" = arr[i];");
             }
         }
-        void setCol(E)(E val) if(__traits(hasMember, RT, col))
+        void setCol(E)(E val) if(__traits(hasMember, RT, col) && (!isArray!E || isSomeString!E))
         {
             mixin("static assert(is(typeof(RT." ~ col ~") == E));"); 
             foreach(i,ref rec; records){
@@ -64,6 +74,25 @@ struct Dataframe(RT){
         }
     }
 
+    /// Adds a new column to dataframe
+    /// realistically creates a new DF type since we are statically typed
+    auto addNewCol(T,string name)(){
+        enum new_type_name = RT.stringof~name;
+        mixin(GenAddStructString!(RT,new_type_name,T.stringof~" "~name~";"));
+        mixin("alias new_type = "~new_type_name~";");
+        Dataframe!new_type newdf;
+        newdf.records.length = this.records.length;
+        alias memberNames = AliasSeq!(RT.tupleof);
+        static foreach (memberName; memberNames)
+        {
+            newdf.setCol!(memberName.stringof)(this.getCol!(memberName.stringof)());
+        }
+        return newdf;
+    }
+
+    /// reads dataframe from a file
+    /// again must predefine columns with struct
+    /// similar to pd.read_table in pandas
     void fromTable(string fn, string sep = "\t",int indexCols = 0, int headerCols = 0){
         alias memberTypes = AliasSeq!(typeof(RT.tupleof));
         alias memberNames = AliasSeq!(RT.tupleof);
@@ -85,6 +114,8 @@ struct Dataframe(RT){
         }
     }
 
+    /// returns string[] of columns 
+    /// similar to df.columns in pandas
     static string[] columns(){
         alias memberNames = AliasSeq!(RT.tupleof);
         string[] ret;
@@ -94,6 +125,8 @@ struct Dataframe(RT){
         return ret;
     }
 
+    /// Returns a groupby object grouped by a string[] of indices 
+    /// similar to df.groupby(["col","col2"]) in pandas
     auto groupby(string[] indices)(){
         enum idx_type_name = GenIndexName!(__traits(identifier,RT),indices);
         static foreach (index; indices)
@@ -129,11 +162,14 @@ struct Dataframe(RT){
         return gby;
     }
 
+    /// returns a dataframe of first n records 
+    /// similar to df.head() in pandas
     auto head(ulong numRows = 5){
         if(numRows > records.length) numRows = records.length;
         return Dataframe!RT(records[0..5].dup);
     }
 
+    /// Converts dataframe to string for printing 
     string toString(){
         auto i = 0;
         string output = "\t"~columns.join("\t")~"\n";
@@ -149,8 +185,20 @@ struct Dataframe(RT){
             output = output[0..$-1] ~'\n';
             i++;
         }
+        output = output[0..$-1];
         return output;
     }
+}
+
+
+/// Base dataframe type
+private struct __BaseDf{
+
+}
+
+/// Create empty dataframe
+auto Dataframe(){
+    return Dataframe!__BaseDf();
 }
 
 private struct testRecord{
@@ -172,4 +220,11 @@ unittest{
     writeln(gby.count);
     df.setCol!"other"("j");
     writeln(df);
+}
+
+unittest{
+    auto df = Dataframe();
+    auto df2 =df.addNewCol!(int,"pos");
+    auto df3 = df2.addNewCol!(int,"pos2");
+    writeln(df3);
 }
